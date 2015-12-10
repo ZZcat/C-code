d79e233c0ebbc54c6a8e185dddc9aa0d2d4c97be
ZZcat <zach@zettervall.com>
2015-12-10 15:23:06 -0600

    Fix a few bugs and finish adding update

--- Main.py
+++ Main.py
@@ -10,14 +10,23 @@ print "Your vesion is ",vesion,"\nThe newest vesion is ", web_vesion
 if vesion == web_vesion:
    print "You have the newest vesion!"
 else:
-   print "You need to update this program"
-   print "Updating..."
-   raw_input()
+   print "###You need to update this program"
+   print "###Updating..."
+   print "\n###Reading lines"
    folder_dir = os.popen("pwd").readlines()
+   print "###Done!!!"
+   print "\n###Cuting varuibal"
    folder_dir = folder_dir[:-10]
+   print "###Done!!!\n\n###Cloning dicrectory"
    com = "git clone https://github.com/ZZcat/C-code.git"
    os.system(com)
+   print "###Done\n\n###Moving directory"
    com = "mv C-code " , folder_dir
+   print "###Done!!!"
+   print "\n###Removing copy folder"
+   com = "rm C-code"
+   print "###Done!!!"
+   
 
 
 
@@ -163,8 +172,8 @@ if __name__ == '__main__':
          
          if event.type == pygame.QUIT:
             run = False
-            sys.quit()
             pygame.quit()
+            sys.quit()
             print "quit"
          elif typing == True and event.type == KEYDOWN:
             if event.unicode.isalpha():
