cddd971126744895632ff6b34889c657657fc928
Spincat360 <ZZcat@users.noreply.github.com>
2016-12-23 12:39:31 -0600

    Bug fixes, update fixs

---
+++ Update.py
@@ -0,0 +1,33 @@
+import os, time
+folder = os.popen("pwd").readlines()
+
+import urllib2
+
+def internet_on():
+    try:
+        urllib2.urlopen('https://github.com/ZZcat/', timeout=1)
+        return True
+    except urllib2.URLError as err: 
+        return False
+
+if internet_on:
+    print "Connected to server"
+    folder = str(folder)
+    print "Loading folder..."
+    folder,trash = folder.split("C-code")
+    trash,folder = folder.split("'")
+
+    print "Removing old directory..."
+    com = "(cd '"+str(folder)+"'&&rm -rf C-code)"
+    os.system(com)
+    print "Updating..."
+    com = '(cd "'+str(folder)+'"&&git clone https://github.com/ZZcat/C-code.git)'
+    os.system(com)
+
+    print "Running script"
+    import Main
+    
+else:
+    print "Unable to connect to ZZcat's github profile"
+    print "Running program in offline mode"
+    import Main
